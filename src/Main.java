220f4c976d7f12559190378d91fbf7272c9b3caf
gyeom123 <gyeomju6536@gmail.com>
2023-02-14 14:00:12 +0900

    githud 업로드 연습 2023-02-14

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("==프로그램 시작==");
+
+		Scanner sc = new Scanner(System.in);
+
+		sc.nextLine();
+
+		String cmd = sc.nextLine();
+
+		System.out.println("입력된 명령어 : " + cmd);
+
+		System.out.println("==프로그램  종류==");
+
+		sc.close();
+	}
+
+}
