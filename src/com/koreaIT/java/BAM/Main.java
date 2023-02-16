77401c62402702910ccbe1eb33f4bd5c07449154
gyeom123 <gyeomju6536@gmail.com>
2023-02-16 14:48:08 +0900

    게시글 수정 기능 구현

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -42,7 +42,9 @@ public class Main {
 				String get_current_date_time = Util.gettine();
 				// Util.gettine() : Util파일에 있는 gettine() ☆현재 날짜시간출력 함수를 실행
 
-				Article article = new Article(id, title, body, get_current_date_time);// 입력받은 정보(종이)를 Article(노트)에 넘겨준다
+				Article article = new Article(id, get_current_date_time, title, body);
+				// 입력받은 정보(종이)를 Article(노트)에 넘겨준다
+				
 				articles.add(article);
 				// 정보를 저장한 article(노트)를 articles(책장)에 넣어서(.add) 정보를 저장하겠다
 				// 함수가 끝나면 정보가 사라지는 전역변수이므로 함수가 끝나기 전에 저장
@@ -56,10 +58,10 @@ public class Main {
 					continue;
 				}
 
-				System.out.println("  번호 ㅣ   제목  ");
+				System.out.println("  번호 ㅣ   제목	ㅣ	날짜");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
-					System.out.printf("   %d   ㅣ    %s  \n", article.id, article.title);
+					System.out.printf("   %d   ㅣ    %s		ㅣ	%s\n", article.id, article.title,article.get_current_date_time);
 
 				}
 
@@ -86,7 +88,7 @@ public class Main {
 				}
 
 				System.out.printf("번호 : %d\n", foundArticle.id);
-				System.out.printf("날짜 : %s\n", foundArticle.get_current_date_time);
+				System.out.printf("날짜 : %s\n", foundArticle.get_current_date_time.substring(0 , 10)); //substring : 출력을 짤라주는 함수
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
 
@@ -116,6 +118,38 @@ public class Main {
 
 				System.out.printf("%d번 게시물이 삭제되었습니다.\n", id);
 
+			} else if (cmd.startsWith("article modify ")) {
+
+				String[] cmdBits = cmd.split(" ");
+				int id = Integer.parseInt(cmdBits[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (id == article.id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물이 없습니다.\n", id);
+					continue;
+				}
+
+				System.out.printf("수정할 제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("수정할 내용 : ");
+				String body = sc.nextLine();
+
+				foundArticle.title = title;
+				foundArticle.body = body;
+				
+				System.out.printf("%d번 게시물이 수정되었습니다.\n", id);
+
+
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다.");
 			}
@@ -130,12 +164,12 @@ public class Main {
 }
 
 class Article {
-	int id; // 고유번호
+	int id; // 게시글의 고유번호
 	String title; // 게시글 제목
 	String body; // 게시글 내용
-	String get_current_date_time; // 게시글 작성시간
+	String get_current_date_time; // 게시글 작성 날짜,시간
 
-	Article(int id, String title, String body, String get_current_date_time) {
+	Article(int id,String get_current_date_time, String title, String body) {
 		// 외부에서 받아온 내용을 저장
 		this.id = id;
 		this.title = title;
--- src/com/koreaIT/java/BAM/Util.java
+++ src/com/koreaIT/java/BAM/Util.java
@@ -6,14 +6,16 @@ import java.util.Date;
 public class Util {
 
 	public static String gettine() {
-		//현재 날짜 시간 출력 
+		//현재 날짜 시간 출력 함수
 		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
+		//yyyy : 년도   MM : 월   dd : 일   HH : 시간   mm : 분  ss  :초
 		Date time = new Date();
 
-		String time1 = format1.format(time);
-
-		return time1;
+	//	String time1 = format1.format(time);
+    //	return time1;
+		
+		return format1.format(time);
+	
 	}
 
 }
